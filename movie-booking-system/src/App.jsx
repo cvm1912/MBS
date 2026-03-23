8fb006d6e15bb44894d7041bf16e4fca6c2c233f
cvm1912 <rajshivam1912@gmail.com>
2026-03-23 17:40:32 +0530

    features: routes added

--- movie-booking-system/src/App.jsx
+++ movie-booking-system/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Movie from './pages/Movie'
+import Seatlayout from './pages/Seatlayout'
+import Favourite from './pages/Favourite'
+import MovieDetails from './pages/MovieDetails'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-5xl'>hello world</h1>
-    </div>
+   <>
+    <Navbar/>
+      <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/movie-details' element={<MovieDetails/>}/>
+       <Route path='/movie' element={<Movie/>}/>
+       <Route path='/seats' element={<Seatlayout/>}/>
+       <Route path='/favourite' elemenent= {<Favourite/>}/>
+    </Routes>
+    <Footer/>
+   </>
   )
 }
 
---
+++ movie-booking-system/src/pages/Seatlayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Seatlayout = () => {
+  return (
+    <div>
+      <h1>seat layout </h1>
+    </div>
+  )
+}
+
+export default Seatlayout
