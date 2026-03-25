fc7db423ecf6b33d84d053f9cda287c5d8395c5c
cvm1912 <rajshivam1912@gmail.com>
2026-03-25 18:59:13 +0530

    navs

---
+++ movie-booking-system/src/Components/Navbars/Logins.jsx
@@ -0,0 +1,25 @@
+import { MenuIcon, SearchIcon } from 'lucide-react'
+import React from 'react'
+
+const Logins = () => {
+  return (
+    <div className='flex gap-2'>
+      <div className='flex gap-4 sm:px-5 sm:py-2'>
+         <SearchIcon className='h-8 w-8'/>
+         <button className='text-2xl'>Login</button>
+      </div>
+      <MenuIcon className='md:hidden h-8 w-8'/>
+    </div>
+  )
+}
+
+export default Logins
+
+
+//   <div className="flex items-center gap-8 cursor-pointer ">
+//         <Search className="w-6 h-6" />
+//         <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
+//           Login
+//         </button>
+//       </div>
+//       <MenuIcon className="md:hidden w-8 h-8 cursor-pointer" />
--- movie-booking-system/src/Components/Navbars/Navbar.jsx
+++ movie-booking-system/src/Components/Navbars/Navbar.jsx
@@ -4,21 +4,16 @@ import Logo from "./Logo";
 import { MenuIcon, Search, XIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import MenuBar from "./MenuBar";
+import Logins from "./Logins";
 
 const Navbar = () => {
   return (
     <div className="fixed top-0 left- w-full  py-6 px-6 lg:px-36 md:px-16  flex items-center justify-between">
       <Logo assets={assets.logo} />
-
       <MenuBar/>
+      <Logins/>
 
-      <div className="flex items-center gap-8 cursor-pointer ">
-        <Search className="w-6 h-6" />
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
-      </div>
-      <MenuIcon className="md:hidden w-8 h-8 cursor-pointer" />
+    
     </div>
   );
 };
